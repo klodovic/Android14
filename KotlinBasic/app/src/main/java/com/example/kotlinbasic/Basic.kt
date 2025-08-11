e97bf8ab8f1b770d0666e9bfedb93ce2ffb3aaf9
Josip Smoljić <klodovic@gmail.com>
2025-08-11 17:38:20 +0200

    Kotlin basic - update
    Functions and classes

--- KotlinBasic/app/src/main/java/com/example/kotlinbasic/Basic.kt
+++ KotlinBasic/app/src/main/java/com/example/kotlinbasic/Basic.kt
@@ -2,6 +2,16 @@ package com.example.kotlinbasic
 
 import java.util.Locale
 
+
+data class CoffeeDetails(
+    val sugarCount: Int,
+    val name: String,
+    val size: String,
+    val creamAmount: Int
+)
+
+
+
 fun main(){
     // 1. integer
     //val -> for const if never changes (Kotlin recommendation for all variables)
@@ -24,8 +34,8 @@ fun main(){
     println(userAge)
 
     //3. Boolean
-    val myTrue : Boolean = true
-    val myFalse : Boolean = false
+    val myTrue = true
+    val myFalse = false
     println(myTrue || myFalse)
     println(myTrue && myFalse)
     println(!myTrue)
@@ -42,36 +52,53 @@ fun main(){
 
     //6. if else statements
     val ages = 40
-    if (ages == 42){
-        println(true)
-    }else{
-        println(false)
-    }
+    if (ages == 42) println(true) else println(false)
 
 
     //user input
-//    println("Insert your age:")
-//    val input = readln().toInt()
-//    if (input == 40){
-//        println(input)
-//    }else{
-//        println(false)
-//    }
-//println("End")
+    println("Insert your age:")
+    val input = readln().toInt()
+    if (input == 40){
+        println(input)
+    }else{
+        println(false)
+    }
+    println("End")
 
     var userInput = readln()
     while (userInput == "1"){
         println("While loop executed")
         userInput = readln()
     }
-println("Loop is done")
+    println("Loop is done")
+
+
+    //7. Functions
+    //7.1 functions without return value
+    askCoffeeDetails()
+
+    //7.2 function with return value
+    println("Enter number 1")
+    val num1 = readln().toInt()
+    println("Enter number 2")
+    val num2 = readln().toInt()
+
+    val addResult = add(num1, num2)
+    val divideResult = divide(num1, num2)
+
+    println(addResult)
+    println(divideResult)
 
 
 
+    // Classes
+    val daisy = Dog("Daisy", breed = "Poodle")
+    println("${daisy.name} is a ${daisy.breed}")
 
 
 
-
+    val coffeeForDenis = CoffeeDetails(0, "Dennis", "xxl", 1)
+    makeCoffee(coffeeForDenis)
 
 }
 
@@ -80,3 +107,42 @@ println("Loop is done")
 
 
 
+/// Functions definition
+fun makeCoffee(coffeeDetails: CoffeeDetails){
+    if (coffeeDetails.sugarCount == 1){
+        println("Coffee with ${coffeeDetails.sugarCount} " +
+                "spoon of sugar for ${coffeeDetails.name}" +
+                "and cream: ${coffeeDetails.creamAmount}"
+        )
+    }else if(coffeeDetails.sugarCount == 0){
+        println("Coffee with no sugar for ${coffeeDetails.name}" +
+                "and cream: ${coffeeDetails.creamAmount}"
+        )
+    }else{
+        println("Coffee with ${coffeeDetails.sugarCount} " +
+                "spoons of sugar for ${coffeeDetails.name}" +
+                "and cream: ${coffeeDetails.creamAmount}"
+        )
+    }
+}
+
+
+fun askCoffeeDetails(){
+    println("Who is this coffee for?")
+    val name = readln()
+    println("How many pieces of sugar do you want?")
+    val sugarCount = readln().toInt()
+    //makeCoffee(sugarCount, name)
+}
+
+
+// function with return value
+fun add(num1: Int, num2:Int): Int {
+    val result = num1 + num2
+    return result
+}
+
+fun divide(num1: Int, num2: Int): Double {
+    val result : Double = (num1 / num2).toDouble()
+    return result
+}
