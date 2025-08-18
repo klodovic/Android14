a2e38629e75c2d087c30ff7ba5ac96b4cb97ef3b
Josip Smoljić <klodovic@gmail.com>
2025-08-18 19:46:58 +0200

    Navigation
    Implementation of a navigation

---
+++ Navigation/app/src/main/java/com/example/navigation/SecondScreen.kt
@@ -0,0 +1,41 @@
+package com.example.navigation
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun SecondScreen(
+    name: String,
+    modifier: Modifier = Modifier,
+    navigateToFirstScreen:(String) -> Unit)
+{
+
+    Column(
+        modifier = modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+    {
+        Text("This is the Second Screen", fontSize = 24.sp)
+
+        Text("Welcome: $name", fontSize = 24.sp)
+
+        Button(onClick = {
+            navigateToFirstScreen(name)
+        })
+        {
+            Text("Go to First screen")
+        }
+    }
+}
